3a5cbd2fa82637b6a1650cfd14b5017f2cc5bb2f
Bill Chen <billchen@Bills-MacBook-Pro-2.local>
2023-01-20 12:39:42 -0500

    Added function to allow user to input HDL result

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,5 +12,10 @@ def interface():
             keep_running = False
     
     print('Program Ending')
+
+def HDL_input():
+    HDL_value = input("Enter the HDL result: ")
+    HDL_value = int(HDL_value)
+    return HDL_value
     
 interface()
