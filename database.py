8eeeea0dd458f22ce96f2284118a6acb2d8b08c9
Bill Chen <billchen@Bills-MacBook-Pro-2.local>
2023-02-01 12:42:04 -0500

    able to retrieve patient data from database

--- database.py
+++ database.py
@@ -4,10 +4,25 @@ def create_patient_entry(patient_name, patient_mrn, patient_age):
 
 def main_driver():
     db = []
-    db.append(create_patient_entry("Jerry Yang", 69, 23))
-    db.append(create_patient_entry("Vivian Fei", 68, 23))
-    db.append(create_patient_entry("Bill Chen", 67, 22))
+    db.append(create_patient_entry("Jerry Yang", 120, 23))
+    db.append(create_patient_entry("Vivian Fei", 60, 23))
+    db.append(create_patient_entry("Jessilyn Dunn", 67, 36))
     print(db)
+    print("Get patient Jerry")
+    mrn_to_find = 120
+    found_patient = get_patient_entry(db, mrn_to_find)
+    if found_patient is False:
+        print(f"Patient mrn {mrn_to_find} not found")
+    else:
+        print(found_patient)
+
+def get_patient_entry(db, mrn_to_find):
+    for patient in db:
+        if patient[1] == mrn_to_find:
+            return patient
+    
+    return False
+        
 
 if __name__ == "__main__":
     main_driver()
