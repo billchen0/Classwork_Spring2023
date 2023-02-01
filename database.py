a0a178a2a308143f0cff8731d2e1e3e6fd5ea9bd
Bill Chen <billchen@Bills-MacBook-Pro-2.local>
2023-02-01 13:00:59 -0500

    wrote a function to print the current room number of all patients

--- database.py
+++ database.py
@@ -1,27 +1,41 @@
 def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age]
+    new_patient = [patient_name, patient_mrn, patient_age, []]
     return new_patient
 
 def main_driver():
     db = []
-    db.append(create_patient_entry("Jerry Yang", 120, 23))
-    db.append(create_patient_entry("Vivian Fei", 60, 23))
-    db.append(create_patient_entry("Jessilyn Dunn", 67, 36))
+    db.append(create_patient_entry("Jerry Yang", 1, 23))
+    db.append(create_patient_entry("Vivian Fei", 2, 23))
+    db.append(create_patient_entry("Jessilyn Dunn", 3, 36))
     print(db)
-    print("Get patient Jerry")
-    mrn_to_find = 120
-    found_patient = get_patient_entry(db, mrn_to_find)
-    if found_patient is False:
-        print(f"Patient mrn {mrn_to_find} not found")
-    else:
-        print(found_patient)
+    
+    add_test_to_patient(db, 1, "HDL", 120)
+    add_test_to_patient(db, 2, "LDL", 100)
+    room_numbers = ["103", "232", "333"]
+    print(db)
+    print_directory(db, room_numbers)
+
+def print_directory(db, room_numbers):
+    for i, patient in enumerate(db):
+        print(f"Patient {patient[0]} is in room {room_numbers[i]}")
+
+    for patient, rn in zip(db, room_numbers):
+        print(f"Patient {patient[0]} is in room {rn}")
 
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
         if patient[1] == mrn_to_find:
             return patient
-    
     return False
+
+def add_test_to_patient(db, mrn_to_find, test_name, test_value):
+    patient = get_patient_entry(db, mrn_to_find)
+    if patient == False:
+        print("Invalid Entry")
+    else:
+        patient[3].append([test_name, test_value])
+    return
+
         
 
 if __name__ == "__main__":
