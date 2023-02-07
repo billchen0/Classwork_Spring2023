f980d791bf6d385d40f46af66c856cdb61fdeb10
bc299 <bc299@duke.edu>
2023-02-07 10:07:42 -0500

    developed the function for linear and ran 3 test cases with pytest parametrize that covers a horizontal line

--- test_linear.py
+++ test_linear.py
@@ -1,12 +1,12 @@
 import pytest 
 
-@pytest.mark.parametrize("coord1", "coord2", "x", "y",
-    [
-        (),
-        (),
-        ()
-    ]
-)
+testdata = [
+    ((1, 2), (2, 2), 5, 2),
+    ((0, 0), (1, 1), 3, 3),
+    ((0, 0), (2, 1), 4, 2),
+]
+
+@pytest.mark.parametrize("coord1, coord2, x, y", testdata)
 def test_linear(coord1, coord2, x, y):
     from linear import linear
 
